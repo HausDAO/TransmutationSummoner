9f4f43c570c75867c44652873d7d57e2de0851bc
Dekan <dekanbro@gmail.com>
2021-03-02 09:19:36 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# MinionSummoner
+Summon All the Minions
+
+## Summoning 
+Summon a minion for your DAO, give it a name, just 'cause this is a vanilla minion (the OG) doesn't mean it doesn't deserve some spice. 
+
+Summoning a minion is easy. The only arguments the summon function takes are the address of the moloch it will minion for (e.g. it's parent DAO) and a description, which can be a name or other description for this minion's purpose. The summon function emits a SummonMinion event where you can grab the new minion's address.
+
+Details of summoned minions can be looked up in the minions mapping, which will allow you to search by minion address and retrieve information about the minion's description and the Moloch it serves. 
+
+## Transmutation
+
+> "Moloch whose blood is running money!"
+
+Transmutation is a contract that enables the process for HausDAO funding proposals.
+The `distributionToken` is slowly traded into the Dao as `capitalToken` is used.
+
+The Transmutation contract holds `distributionToken` (e.g. HAUS) which can only be distributed through Dao proposals (with owner approval to transfer as a fail-safe).
+The `distributionToken` is swapped with the Dao for `capitalToken` (held in the Dao) through moloch proposals.
+If passed, these proposals will send `distributionToken` as tribute to the Dao and allow some `applicant` address to withdraw the proposed amount of `capitalToken`.
+The rate of `distributionToken` to `capitalToken` for a valid proposal will be set based on social consensus within the Dao.
+
+### Comments and known issues
+
+When a proposal is made through the Transmutation contract, the proposed amount of `distributionToken` is immediately locked up in the Dao.
+After a failed or cancelled proposal through the Transmutation contract, `Transmutation.withdrawdistributionToken` must be called to retrieve these tokens.
+As a result of this dynamic, any address that holds voting shares in the moloch can cancel any Transmutation proposal until the proposal is sponsored.
+Members with voting shares can also atomically call `Transmutation.propose` followed by `Moloch.sponsorProposal`, which would allow locking up any amount of `distributionToken` until the proposal is voted down.
+The expected response to this would be to `guildKick` the proposer from the Dao, but the member would retain their voting shares until the `guildKick` proposal was processed, meaning they could likely perform the same attack a second time immediately after the griefing proposal is processed.
+In short, a rogue Dao member could lock all of the Transmutation contract's `distributionToken` for up to two Dao voting periods.
+
+## Constructor
+
+    constructor(
+        address _moloch,
+        address _distributionToken,
+        address _capitalToken,
+        address _owner
+    )
+
+* The constructor takes The DAO address (_moloch).
+* The token (_distributionToken) which is held by this contract and is used to replace funds when the payment token is requested in a proposal.
+* The token address (_capitalToken) that is considered the 'payment token' for the round .
+* The owner of the contract (_owner). Should be a Minion
+
+Both the DAO and the Minion are approved to move funds from this contract. The DAO must be approved to so this contract can make tribute to it through a proposal. And the Minion Owner is approved so tokens can be pulled out of this contract by a DAO proposal.
+
+## Public Functions
+
+`  function withdrawdistributionToken()`
+
+this is for the case where the dao sends some tokens to this contract through a proposal. This contract must be able to call withdrawBalance
+
+## Member Only Functions
+
+`  function cancel(uint256 _proposalId) external`
+
+if a proposal is made in error this allows any DAO member to cancel it. Avoiding having to sponsored the proposal and then vote it down.
+
+    function propose(
+        address _applicant,
+        uint256 _giveAmt,
+        uint256 _getAmt,
+        string calldata _details
+    )
+
+This is a wrapper around the moloch submitProposal function. It will ask for payment in the capitalToken and give tribute in the distributionToken. _details is a param to collect a short desctiption and is substituted with the constant TRANSMUTATION_DETAILS which is used for front ends to display some details about the proposal.
+
+## Events
+
+    event Propose(uint256 proposalId, address sender);
+    event Cancel(uint256 proposalId, address sender);
+    event Deploy(
+        address moloch,
+        address distributionToken,
+        address capitalToken,
+        address owner
+    );
+
+## Deployments
+
+xDAI - 
+
+kovan - 
+
+rinkeby - 
+
+mainnet - 
---
+++ TransmutationFactory.sol
@@ -0,0 +1,297 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.7.5;
+
+interface IERC20 {
+    // brief interface for moloch erc20 token txs
+    function balanceOf(address who) external view returns (uint256);
+
+    function transfer(address to, uint256 value) external returns (bool);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) external returns (bool);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+}
+
+library SafeMath {
+    // arithmetic wrapper for unit under/overflow check
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        require(c / a == b);
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0);
+        uint256 c = a / b;
+        return c;
+    }
+}
+
+interface IMOLOCH {
+    // brief interface for moloch dao v2
+
+    function depositToken() external view returns (address);
+
+    function tokenWhitelist(address token) external view returns (bool);
+
+    function getProposalFlags(uint256 proposalId)
+        external
+        view
+        returns (bool[6] memory);
+
+    function members(address user)
+        external
+        view
+        returns (
+            address,
+            uint256,
+            uint256,
+            bool,
+            uint256,
+            uint256
+        );
+
+    function userTokenBalances(address user, address token)
+        external
+        view
+        returns (uint256);
+
+    function cancelProposal(uint256 proposalId) external;
+
+    function submitProposal(
+        address applicant,
+        uint256 sharesRequested,
+        uint256 lootRequested,
+        uint256 tributeOffered,
+        address tributeToken,
+        uint256 paymentRequested,
+        address paymentToken,
+        string calldata details
+    ) external returns (uint256);
+
+    function withdrawBalance(address token, uint256 amount) external;
+}
+
+contract Transmutation {
+    using SafeMath for uint256;
+
+    // --- Constants ---
+    uint256 constant MAX_UINT = 2**256 - 1;
+
+    // --- State ---
+    IMOLOCH public moloch;
+    address public distributionToken;
+    address public capitalToken;
+
+    // --- Events ---
+    event Propose(uint256 proposalId, address sender);
+    event Cancel(uint256 proposalId, address sender);
+    event Deploy(
+        address moloch,
+        address distributionToken,
+        address capitalToken,
+        address owner
+    );
+
+    // --- Modifiers ---
+    modifier memberOnly() {
+        require(isMember(msg.sender), "Transmutation::not-member");
+        _;
+    }
+
+    // --- Constructor ---
+    /**
+     * @dev Constructor
+     * @param _moloch The molochDao to propose token swaps with
+     * @param _distributionToken The token to use as Moloch proposal tributeToken
+     * @param _getToken The token to use as Moloch proposal paymentToken
+     * @param _owner Address approved to transfer this contract's _distributionToken
+     */
+    function init(
+        address _moloch,
+        address _distributionToken,
+        address _getToken,
+        address _owner
+    ) public {
+        moloch = IMOLOCH(_moloch);
+        capitalToken = _getToken;
+        distributionToken = _distributionToken;
+
+        emit Deploy(_moloch, _distributionToken, _getToken, _owner);
+
+        // approve moloch and owner to transfer our distributionToken
+        require(
+            IERC20(_distributionToken).approve(_moloch, MAX_UINT),
+            "Transmutation::approval-failure"
+        );
+        require(
+            IERC20(_distributionToken).approve(_owner, MAX_UINT),
+            "Transmutation::approval-failure"
+        );
+    }
+
+    // --- Public functions ---
+
+    /**
+     * @dev Triggers a withdraw of distributionToken from the Moloch
+     */
+    function withdrawdistributionToken() public {
+        moloch.withdrawBalance(
+            distributionToken,
+            moloch.userTokenBalances(address(this), distributionToken)
+        );
+    }
+
+    // --- Member-only functions ---
+
+    /**
+     * @dev Makes a proposal taking tribute from this contract in the form of
+     * distributionToken and sending _getToken to _applicant as proposal payment
+     * @param _applicant Recipient of the proposal's _getToken from the moloch
+     * @param _giveAmt Amount of _distributionToken to swap for _getAmt of _getToken
+     * @param _getAmt Amount of _getToken to swap for _giveAmt of _distributionToken
+     * @param _details Proposal details
+     */
+    function propose(
+        address _applicant,
+        uint256 _giveAmt,
+        uint256 _getAmt,
+        string calldata _details
+    ) external memberOnly returns (uint256) {
+        // this contract cannot accept any tokens except _distributionToken
+        require(
+            _applicant != address(this),
+            "Transmutation::invalid-applicant"
+        );
+
+        // make a Moloch proposal with _distributionToken as tributeToken and
+        // _getToken as paymentToken
+        uint256 proposalId =
+            moloch.submitProposal(
+                _applicant,
+                0,
+                0,
+                _giveAmt,
+                distributionToken,
+                _getAmt,
+                capitalToken,
+                _details
+            );
+
+        emit Propose(proposalId, msg.sender);
+        return proposalId;
+    }
+
+    /**
+     * @dev Cancel a Moloch proposal. Can be called by any dao member
+     * @param _proposalId The id of the proposal to cancel
+     */
+    function cancel(uint256 _proposalId) external memberOnly {
+        emit Cancel(_proposalId, msg.sender);
+        moloch.cancelProposal(_proposalId);
+    }
+
+    // --- View functions ---
+
+    /**
+     * @dev Returns true if usr has voting shares in the Moloch
+     * @param usr Address to check membership of
+     */
+    function isMember(address usr) public view returns (bool) {
+        (, uint256 shares, , , , ) = moloch.members(usr);
+        return shares > 0;
+    }
+
+    receive() external payable {}
+}
+
+/*
+The MIT License (MIT)
+Copyright (c) 2018 Murray Software, LLC.
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+contract CloneFactory {
+    function createClone(address payable target)
+        internal
+        returns (address payable result)
+    {
+        // eip-1167 proxy pattern adapted for payable minion
+        bytes20 targetBytes = bytes20(target);
+        assembly {
+            let clone := mload(0x40)
+            mstore(
+                clone,
+                0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000
+            )
+            mstore(add(clone, 0x14), targetBytes)
+            mstore(
+                add(clone, 0x28),
+                0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000
+            )
+            result := create(0, clone, 0x37)
+        }
+    }
+}
+
+contract TransmutationFactory is CloneFactory {
+    address payable public immutable template; // fixed template for minion using eip-1167 proxy pattern
+    address[] public minionList;
+    mapping(address => AMinion) public minions;
+
+    event SummonMinion(
+        address indexed minion,
+        address indexed moloch,
+        string details,
+        string minionType
+    );
+
+    struct AMinion {
+        address moloch;
+        string details;
+    }
+
+    constructor(address payable _template) {
+        template = _template;
+    }
+
+    function summonMinion(
+        address moloch,
+        string memory details,
+        address distributionToken,
+        address capitalToken,
+        address owner // dao vanilla minion
+    ) external returns (address) {
+        Transmutation minion = Transmutation(createClone(template));
+
+        minion.init(moloch, distributionToken, capitalToken, owner);
+        string memory minionType = "transmutation minion";
+
+        minions[address(minion)] = AMinion(moloch, details);
+        minionList.push(address(minion));
+        emit SummonMinion(address(minion), moloch, details, minionType);
+
+        return (address(minion));
+    }
+}
